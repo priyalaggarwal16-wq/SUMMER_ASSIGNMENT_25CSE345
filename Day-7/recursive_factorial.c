092f1a49a11533081f2ce2a97ae536bc0a328695
PRIYAL AGGARWAL <priyal16@PRIYALs-MacBook-Air.local>
2026-06-10 14:09:32 +0530

    Add Day-7

---
+++ Day-7/recursive_factorial.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+long long factorial(int n)
+{
+    if(n==0||n==1)
+        return 1;
+         else
+        return n*factorial(n-1);
+}
+int main()
+{
+    int n;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    if(n<0)
+    {
+        printf("Factorial is not defined for negative numbers.");
+    }
+    else
+    {
+        printf("Factorial of %d is %lld", n, factorial(n));
+    }
+    return 0;
+
+}
