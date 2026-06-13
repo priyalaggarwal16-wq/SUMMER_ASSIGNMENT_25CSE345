49b1333361544826eeb8a9c584b64906af0b9592
PRIYAL AGGARWAL <priyal16@PRIYALs-MacBook-Air.local>
2026-06-13 14:42:46 +0530

    Add Day-13

---
+++ Day-13/count_even_and_odd_elements.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d\n", odd);
+
+    return 0;
+}
---
+++ Day-13/find_largest_and_smallest_element.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, max, min;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    max = a[0];
+    min = a[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] > max)
+            max = a[i];
+
+        if(a[i] < min)
+            min = a[i];
+    }
+
+    printf("Largest element = %d\n", max);
+    printf("Smallest element = %d\n", min);
+
+    return 0;
+}
